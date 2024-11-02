6241582d9c2c9ed276e02e70b53f82343d3123f8
Cihan BAS <cihanbas07@gmail.com>
2024-11-02 12:44:00 +0300

    ListItem

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,47 @@
+import { Image, StyleSheet, Text, View } from "react-native"
+import Button from "./Button"
+import { colors, item_width, padding } from "../utils/constants"
+import { ItemUrun } from "../types/products"
+
+const RenderItem = ({ urun }: ItemUrun) => {
+
+    return <View style={styles.item_container}  >
+        <Image source={{ uri: urun.image }} style={styles.item_image} />
+        <Text numberOfLines={2} style={styles.item_title}>
+            {urun.title}
+        </Text>
+        <Text style={styles.item_price}>
+            ${urun.price}
+        </Text>
+        <Text>
+            {urun.rating.rate} {urun.rating.count}
+        </Text>
+        <Button text='Sepete Ekle' />
+    </View>
+}
+export default RenderItem
+const styles = StyleSheet.create({
+    item_container: {
+        flex: 1,
+        marginBottom: 8,
+        rowGap: 8,
+        margin: padding
+    },
+    item_image: {
+        height: item_width,
+        width: item_width,
+        resizeMode: 'contain',
+        backgroundColor: colors.white
+    },
+    item_title: {
+        fontSize: 16,
+        color: colors.primary
+    }, item_price: {
+
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'green'
+    }
+
+
+})
