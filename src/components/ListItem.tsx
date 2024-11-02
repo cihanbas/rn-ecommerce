c9bf128308f98bc9b011b0ad36096750ef3c3f44
Cihan BAS <cihanbas07@gmail.com>
2024-11-02 12:53:38 +0300

    list item price ui update

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,22 +4,24 @@ import { colors, item_width, padding } from "../utils/constants"
 import { ItemUrun } from "../types/products"
 
 const RenderItem = ({ urun }: ItemUrun) => {
-
     return <View style={styles.item_container}  >
         <Image source={{ uri: urun.image }} style={styles.item_image} />
         <Text numberOfLines={2} style={styles.item_title}>
             {urun.title}
         </Text>
-        <Text style={styles.item_price}>
-            ${urun.price}
-        </Text>
-        <Text>
-            {urun.rating.rate} {urun.rating.count}
-        </Text>
+        <View style={styles.item_price_rate}>
+            <Text style={styles.item_price}>
+                ${urun.price}
+            </Text>
+            <Text>
+                {urun.rating.count} {urun.rating.rate}
+            </Text>
+        </View>
         <Button text='Sepete Ekle' />
     </View>
 }
 export default RenderItem
+
 const styles = StyleSheet.create({
     item_container: {
         flex: 1,
@@ -36,12 +38,15 @@ const styles = StyleSheet.create({
     item_title: {
         fontSize: 16,
         color: colors.primary
-    }, item_price: {
-
+    },
+    item_price: {
         fontWeight: 'bold',
         fontSize: 16,
         color: 'green'
+    },
+    item_price_rate: {
+        flexDirection: 'row', justifyContent: 'space-between',
+        alignItems: 'center'
     }
-
 
 })
